bc2827c96e06c5a35c161148b7e6feca1c726b4d
yossi <54272821+Apakottur@users.noreply.github.com>
2026-07-15 10:26:06 +0000

    comment

--- src/pgmig/__init__.py
+++ src/pgmig/__init__.py
@@ -1,4 +1,8 @@
 from pgmig._errors import PgmigError, UnsupportedChangeError
 from pgmig.api import generate
 
-__all__ = ["PgmigError", "UnsupportedChangeError", "generate"]
+__all__ = [
+    "PgmigError",
+    "UnsupportedChangeError",
+    "generate",
+]
